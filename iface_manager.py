47fea4a32a88ca019f68e309db72897c5ef02741
Jaume Serrats <jaume.serrats@gmail.com>
2018-06-28 18:53:53 +0200

    refactored and improved

--- iface_manager.py
+++ iface_manager.py
@@ -3,18 +3,30 @@ from os import system
 
 
 def start_mon_iface():
-    iface, mon_mac, initial_interfaces = choose_interface()
-    system('sudo airmon-ng start ' + iface)
-    final_interfaces = netifaces.interfaces()
-    new_interface = list(set(final_interfaces) - set(initial_interfaces))[0]
-    return new_interface, mon_mac
+    actual_mon = get_mon_iface()
+
+    if actual_mon is False:
+        iface, mon_mac, initial_interfaces = choose_iface()
+        system('sudo airmon-ng start ' + iface)
+        final_interfaces = netifaces.interfaces()
+        new_interface = list(set(final_interfaces) - set(initial_interfaces))[0]
+        return new_interface, mon_mac
+    else:
+        return actual_mon, netifaces.ifaddresses(actual_mon)[netifaces.AF_LINK][0]['addr']
 
 
 def stop_mon_iface():
-    system('sudo airmon-ng stop mon0')
+    system('sudo airmon-ng stop ' + get_mon_iface())
 
 
-def choose_interface():
+def get_mon_iface():
+    for iface in netifaces.interfaces():
+        if "mon" in iface:
+            return iface
+    return False
+
+
+def choose_iface():
     initial_interfaces = netifaces.interfaces()
 
     for count, interface in enumerate(initial_interfaces):
@@ -35,11 +47,13 @@ def choose_interface():
 
 
 if __name__ == '__main__':
-    if 'mon' in netifaces.interfaces()[-1]:
-        print('Monitor Mode Active - Disabling it..')
-        system('sudo airmon-ng stop ' + netifaces.interfaces()[-1])
+
+    actual_mon = get_mon_iface()
+
+    if actual_mon is False:
+        print('Choose interface to monitor')
+        interface, interface_mac, null = choose_iface()
+        system('sudo airmon-ng start ' + interface)
 
     else:
-        print('Choose interface to monitor')
-        interface, interface_mac, null = choose_interface()
-        system('sudo airmon-ng start ' + interface)
+        system('sudo airmon-ng stop ' + actual_mon)
